3ce597de46591255c3b5543fb988af948f10fc21
VloneDSb <kozachekivan60@gmail.com>
2023-04-26 18:13:55 +0300

    phone 1.1.1

--- phone_call.py
+++ phone_call.py
@@ -5,8 +5,8 @@ class phone:
         self.model = model 
         self.weight = weight
     
-    def print_data(self) -> str: # выводит информацию
-        return f'Телефон {self.model}, номер {self.number}, вес {self.weight} ' # Вес чего? Бананов??
+    def __str__(self) -> str: # выводит информацию
+        return f'Телефон {self.model}, номер {self.number}, вес: {self.weight} ' # Вес чего? Бананов??
     
     def recive_call(self, name): # выводит информацию 
         self.name = name
@@ -17,17 +17,17 @@ class phone:
     
 phone1 = phone('+79157776989', 'Xiaomi redmi pro', '170')
 phone1.recive_call('Vlone') 
-print(phone1.print_data())
+print(phone1)
 print(phone1.get_number())
 print(' ')
 
 phone2 = phone('+99106669889','Iphone X')
 phone2.recive_call('Sayori') 
-print(phone2.print_data())
+print(phone2)
 print(phone2.get_number())
 print(' ')
 
 phone3 = phone('+79109992233')
 phone3.recive_call('Qwerez')
-print(phone3.print_data())
+print(phone3)
 print(phone3.get_number())
